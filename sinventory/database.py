7c33dccec319d7b7e1960b111d4c7d90c5ab67de
Dhiman Ghosh <vicky.wb@gmail.com>
2018-07-10 00:59:08 +0530

    front-end enhancements

--- sinventory/database.py
+++ sinventory/database.py
@@ -1,5 +1,38 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#DATABASE_URL = os.environ['DATABASE_URL']
+
+class Database:
+    conn = None
+
+    def __init__(self, table_name, database_url=os.environ['DATABASE_URL']):
+        print("DB: " + database_url)
+        if Database.conn is None:
+            Database.conn = psycopg2.connect(database_url, sslmode='require')
+        self._table_name = table_name
+    def _insert(self):
+        values = list()
+        query = None
+        dict_ = self.__dict__.copy()
+        dict_.pop('_table_name')
+        print(dict_)
+        for key, value in dict_.items():
+            values.append(value)
+        values = str(str(values).rstrip(']')).lstrip( '[' )
+        query = "INSERT INTO " + self._table_name + " VALUES (" + values + ");"
+        #print("Query: " + query)
+        self.__execute(query)
+    def _update(self):
+        pass
+
+    def _delete(self):
+        pass
+
+    def _select(self):
+        pass
+
+    def __execute(self, query):
+        cur = Database.conn.cursor()
+        cur.execute(query)
+        Database.conn.commit()
--- sinventory/server.py
+++ sinventory/server.py
@@ -1,12 +1,94 @@
 import os
-from flask import Flask, render_template
+import json
+import logging
+from flask import *
 
-pkg_dir = os.path.dirname(os.path.realpath(__file__))
+API = '/api/v1'
+PKG_DIR = os.path.dirname(os.path.realpath(__file__))
 
-app = Flask(__name__, template_folder=pkg_dir + '/../htdocs')
+app = Flask(__name__, template_folder=PKG_DIR + '/../htdocs')
+app.secret_key = 'wanderingcouple1194'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+@app.before_first_request
+def setup_logging():
+  gunicorn_logger = logging.getLogger('gunicorn.error')
+  app.logger.handlers = gunicorn_logger.handlers
+  app.logger.setLevel(gunicorn_logger.level)
 
 @app.route('/')
 def index():
-  login_data = render_template('login.inc')
+  if 'id' in session:
+    data_local = {'8899775544': ('Amul', 'Lassi', '2', '15', '+1')}
+    table_data = render_template('table.inc', data=data_local)
+    return render_template('template.html', main_data=table_data,
+        title="Recent Activity")
+
+  error_no = request.args.get('error', 0)
+  login_data = render_template('login.inc', error=int(error_no))
   return render_template('template.html', main_data=login_data)
 
+@app.route('/auth', methods=['POST'])
+def auth_redirect():
+  pin = request.form['pin']
+  resp = authorize(pin)
+
+  if resp.status_code == 200:
+    session['id'] = pin
+    return redirect(url_for('index'))
+  return redirect(url_for('index') + '?error=1')
+
+@app.route('/logout')
+def logout():
+  session.pop('id', None)
+  return redirect(url_for('index'))
+
+@app.route('/product/<barcode>')
+def product(barcode):
+  # Fetch product goes here
+  product_data={'name': 'Lassi', 'company': 'Amul',
+      'category': 'food', 'price': '15', 'measurement': '100 ml'}
+  product_data = render_template('product.inc',
+      barcode=barcode, data=product_data, action='insert')
+  return render_template('template.html', main_data=product_data,
+      title="Product Database")
+
+@app.route('/stock/<barcode>')
+def stock(barcode):
+  stock_data = render_template('stock.inc', barcode=barcode)
+  return render_template('template.html', main_data=stock_data,
+      title="Product Stock")
+
+# --------------- API ------------------
+@app.route(API + '/auth/<pin>', methods=['GET'])
+def authorize(pin):
+  if pin == '123456':
+    return Response("{'status': 'OK'}")
+  else:
+    return Response("{'status': 'Unauthorized'}", status=401)
+
+@app.route(API + '/product/<action>', methods=['POST', 'PUT', 'PATCH', 'DELETE'])
+def manage_product(action):
+  method = request.method
+  if request.method == 'POST':
+    if action == "insert":
+      method = 'PUT'
+    elif action == "update":
+      method = 'PATCH'
+    elif action == "delete":
+      method = 'DELETE'
+    else:
+      return Response("{'status': 'Bad Request'}", status=400)
+
+  if method == "PUT":
+    return "{'status': 'OK'}"
+  elif method == "PATCH":
+    return "{'status': 'OK'}"
+  elif method == "DELETE":
+    return "{'status': 'OK'}"
+  else:
+    return Response("{'status': 'Internal Server Error'}", status=500)
+
+@app.route(API + '/stock/<barcode>')
+def update_stock(barcode):
+  return {'status': 'OK'}
