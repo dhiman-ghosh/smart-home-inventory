635e1c312288e0a78d87212feb04493ebaa84c16
Sukanya Ray <sukanyaray007@gmail.com>
2018-07-10 15:51:47 +0530

    Add files via upload

--- sinventory/database.py
+++ sinventory/database.py
@@ -7,7 +7,6 @@ class Database:
     conn = None
 
     def __init__(self, table_name, database_url=os.environ['DATABASE_URL']):
-        print("DB: " + database_url)
         if Database.conn is None:
             Database.conn = psycopg2.connect(database_url, sslmode='require')
         self._table_name = table_name
@@ -21,16 +20,24 @@ class Database:
             values.append(value)
         values = str(str(values).rstrip(']')).lstrip( '[' )
         query = "INSERT INTO " + self._table_name + " VALUES (" + values + ");"
-        #print("Query: " + query)
+        print("Query: " + query)
         self.__execute(query)
+
     def _update(self):
         pass
 
     def _delete(self):
-        pass
+        for key, value in __dict__.items():
+            if key is "alexa_id":
+                query = "DELETE FROM " + self._table_name + " WHERE " + key + " = " + value + ");"
+        print("Query: " + query)
+        self.__execute(query)
 
     def _select(self):
-        pass
+        for key, value in __dict__.items():
+            query = "SELECT " + key + " FROM " + self._table_name + ");"
+        print("Query: " + query)
+        self.__execute(query)
 
     def __execute(self, query):
         cur = Database.conn.cursor()
